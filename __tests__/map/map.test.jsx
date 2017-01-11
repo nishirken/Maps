1dd6e42ab7110eea82fd518bbd621aa61c8d949a
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-01-11 10:47:57 +0500

    added css modules

--- __tests__/map/map.test.jsx
+++ __tests__/map/map.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import MAP from '../../src/containers/map';
+import MAP from '../../src/containers/map/map';
 import { MAP_SETTINGS } from '../../src/constants';
 
 describe('Map', () => {
@@ -13,7 +13,7 @@ describe('Map', () => {
   it('Rendered', () => {
     expect(Map.find('.map').length).toBe(1);
   });
-console.log(Map.props)
+
   it('Has an inner Google Maps', () => {
     expect(Map.find('.map').children().length).toBeGreaterThanOrEqual(1);
   });
