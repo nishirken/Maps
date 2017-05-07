2f8b16d199fe6e4090a872475e64c0f143492713
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-05-07 12:09:34 +0500

    sorted methods of list-item-text

--- src/components/list-item-text/list-item-text.jsx
+++ src/components/list-item-text/list-item-text.jsx
@@ -1,30 +1,22 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 
-import propTypes from './list-item-text-proptypes';
 import StyledMarkerName from './styled-marker-name';
 import StyledMarkerNameEditField from './styled-marker-name-edit-field';
 import StyledMarkerCoords from './styled-marker-coords';
 
 export default class ListItemText extends PureComponent {
-    static propTypes = propTypes;
-
-    componentDidUpdate() {
-        if (this.props.editMarkerName && this.input)
-            this.input.focus();
-    }
-
-    onKeyDownMarkerName(e) {
-        if (e.keyCode === 27)
-            this.props.switchEditMarkerName(false);
-
-        if (e.keyCode === 13)
-            this.props.switchEditMarkerName(true);
-    }
-
-    onChangeMarkerName(e) {
-        const value = e.target.value;
-
-        this.props.setNewMarkerName(value);
+    render() {
+        return (
+            <div>
+                {this.renderMarkerName()}
+                <StyledMarkerCoords>
+                    coords: {this.props.markerCoords.get('lat').toFixed(2)},
+                    &nbsp;{this.props.markerCoords.get('lng').toFixed(2)} <br />
+                    №: {this.props.markerNumber}
+                </StyledMarkerCoords>
+            </div>
+        );
     }
 
     renderMarkerName() {
@@ -48,16 +40,34 @@ export default class ListItemText extends PureComponent {
         );
     }
 
-    render() {
-        return (
-            <div>
-                {this.renderMarkerName()}
-                <StyledMarkerCoords>
-                    coords: {this.props.markerCoords.get('lat').toFixed(2)},
-                    &nbsp;{this.props.markerCoords.get('lng').toFixed(2)} <br />
-                    №: {this.props.markerNumber}
-                </StyledMarkerCoords>
-            </div>
-        );
+    onKeyDownMarkerName(e) {
+        if (e.keyCode === 27)
+            this.props.switchEditMarkerName(false);
+
+        if (e.keyCode === 13)
+            this.props.switchEditMarkerName(true);
     }
+
+    onChangeMarkerName(e) {
+        const value = e.target.value;
+
+        this.props.setNewMarkerName(value);
+    }
+
+    componentDidUpdate() {
+        if (this.props.editMarkerName && this.input)
+            this.input.focus();
+    }
+
+    static propTypes = {
+        editMarkerName: PropTypes.bool,
+        markerCoords: ImmutablePropTypes.mapContains({
+            lat: PropTypes.number,
+            lng: PropTypes.number,
+        }),
+        markerName: PropTypes.string,
+        markerNumber: PropTypes.number,
+        setNewMarkerName: PropTypes.func,
+        switchEditMarkerName: PropTypes.func,
+    };
 }
