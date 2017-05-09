0aff22260fdcf111afb7f3ec0f9257ddd3f6a9b5
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-05-09 17:57:28 +0500

    added server side tests

--- __tests__/server/creation-server-store.js
+++ __tests__/server/creation-server-store.js
@@ -9,8 +9,14 @@ describe('Creation server store', () => {
             .reply(200, testServerInitialState);
     });
 
-    it('Should dispatch index', () => {
+    it('Should dispatch all actions', () => {
+        const stateSize = testServerInitialState.size;
+        const markerSize = testServerInitialState.get(0).size;
+        const objectsSize = testServerInitialState.get(0).get('objects').size;
+
         serverStoreDispatch(testServerInitialState, testStore);
+        expect(spy.mock.calls.length)
+            .toBe(stateSize * (markerSize - 1 + objectsSize));
     });
 });
 
